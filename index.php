70973863a02dfdf2f0e4d8caac5dd70c9f838bde
raccoon121 <95743740+raccoon121@users.noreply.github.com>
2021-12-08 01:58:24 -0800

    Update index.php
    aaaa

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ else
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>PHP Application - AWS Elastic Beanstalk</title>
     <meta name="viewport" content="width=device-width">
+	test
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lobster+Two" type="text/css">
     <link rel="icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
     <link rel="shortcut icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
